4b466cfa1748210987dc9f87a25a68d3b8dcf1cf
aljoharah <j70725672@gmail.com>
2024-09-29 15:03:55 +0300

    newb file

---
+++ RepresentMe Team8/Aljoharah ab.swift
@@ -0,0 +1,18 @@
+//
+//  Aljoharah ab.swift
+//  RepresentMe Team8
+//
+//  Created by الجوهرة بنت عبدالله on 26/03/1446 AH.
+//
+
+import SwiftUI
+
+struct Aljoharah_ab: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Aljoharah_ab()
+}
