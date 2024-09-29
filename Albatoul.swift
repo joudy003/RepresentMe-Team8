b0007938e8f07d2bd20d5544ecc2fd85360c3699
Albatoul <ebatoul25@gmail.com>
2024-09-29 15:11:10 +0300

    test

---
+++ Albatoul.swift
@@ -0,0 +1,18 @@
+//
+//  Albatoul.swift
+//  RepresentMe Team8
+//
+//  Created by Albatoul Alhamas on 26/03/1446 AH.
+//
+
+import SwiftUI
+
+struct Albatoul: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Albatoul()
+}
