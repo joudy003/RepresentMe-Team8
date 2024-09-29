1a8c380f87a2c1c742925ec251695301594b0865
Manar Alghamdi <manar.a.gh@hotmail.com>
2024-09-29 15:02:55 +0300

    manar Creation File

---
+++ ManarAlghamdi.swift
@@ -0,0 +1,18 @@
+//
+//  ManarAlghamdi.swift
+//  RepresentMe Team8
+//
+//  Created by Manar Alghmadi on 26/03/1446 AH.
+//
+
+import SwiftUI
+
+struct ManarAlghamdi: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ManarAlghamdi()
+}
