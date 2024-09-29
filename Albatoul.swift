68527eaa00f4627701a69bda1c428fe5fb83e8f6
Albatoul <ebatoul25@gmail.com>
2024-09-29 15:14:21 +0300

    taem8

--- Albatoul.swift
+++ Albatoul.swift
@@ -9,7 +9,7 @@ import SwiftUI
 
 struct Albatoul: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        Text("Hello,team 8" )
     }
 }
 
